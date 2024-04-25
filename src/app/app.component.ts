3a388bd772cc8191c628c05ddaf1690987a71489
tycho.seghers <tychoseghers@icloud.com>
2024-04-25 10:01:10 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { HeroComponent } from "./hero/hero.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FooterComponent, HeroComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'ng-newsApp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeadlinesComponent } from './headlines/component/headlines/headlines.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HeadlinesComponent },
+    { path: ':category', component: HeadlinesComponent }
+];
